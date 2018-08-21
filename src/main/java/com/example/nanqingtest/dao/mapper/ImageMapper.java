b3a27a9c3c4250c3e0a84bcc4a893c702432ca10
43517 <tingjue.du@wuxcl.com>
2018-08-21 19:09:58 +0800

    修复imagemapper错误

--- src/main/java/com/example/nanqingtest/dao/mapper/ImageMapper.java
+++ src/main/java/com/example/nanqingtest/dao/mapper/ImageMapper.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
-
 @Mapper
 public interface ImageMapper {
     int deleteByPrimaryKey(Integer id);
