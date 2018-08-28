ca8e63f5615332487927f0c80e419e49490c389c
罗士程 <dutingjue@163.com>
2018-08-29 00:37:10 +0800

    新建数据库

---
+++ src/main/java/com/example/nanqingtest/model/entity/ArticleT.java
@@ -0,0 +1,51 @@
+package com.example.nanqingtest.model.entity;
+
+import java.io.Serializable;
+
+public class ArticleT implements Serializable {
+    private Integer id;
+
+    private String title;
+
+    private String cover;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover == null ? null : cover.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", title=").append(title);
+        sb.append(", cover=").append(cover);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
